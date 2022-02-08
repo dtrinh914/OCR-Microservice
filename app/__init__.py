19280a7231d573e2c4c314ee8b5bb43ad31a0f8a
dtrinh914 <dtrinh914@gmail.com>
2022-02-07 21:59:14 -0800

    added upload route/ocr processing

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,55 @@
-from flask import Flask
+# Code Reference: https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
+
+import os
+import tempfile
+from werkzeug.utils import secure_filename
+import cv2
+import pytesseract
+
+UPLOAD_FOLDER = tempfile.gettempdir()
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename) -> bool:
+    """Checks if the filename has an allowed extension"""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def ocr(filepath) -> str:
+    """Reads the image at the filepath and returns the OCR text"""
+    img = cv2.imread(filepath)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    custom_config = r'--oem 3 --psm 6'
+    return pytesseract.image_to_string(img, config=custom_config)
 
 
 def create_app() -> Flask:
     """Assembles and returns application."""
     app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['MAX_CONTENT_LENGTH'] = 2 * 1000 * 1000  # set max size to 2MB
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p>"
+    @app.route('/', methods=['POST'])
+    def upload_file():
+        if 'file' not in request.files:
+            return 'Missing key with name of "file" in POST request'
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return 'No file selected'
+
+        if not allowed_file(file.filename):
+            return 'File type not allowed'
+
+        if file:
+            try:
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                return ocr(filepath)
+            except werkzeug.exceptions.RequestEntityTooLarge:
+                return 'File too large'
 
     return app
